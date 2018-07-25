81c1bc0a247b05ac656834ba64e5e3953d917492
vishnuprasaths <41608714+vishnuprasaths@users.noreply.github.com>
2018-07-25 17:36:14 +0530

    component created

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent implements OnInit {
+  toppings = new FormControl();
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+
+  constructor() { }
+    ngOnInit() {
+      
+      
+    }
+
+}
