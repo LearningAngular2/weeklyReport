e244ea4e505ed6a2fe6458073af6f544099435ab
vishnuprasaths <41608714+vishnuprasaths@users.noreply.github.com>
2018-07-25 18:51:35 +0530

    dashboard page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatInputModule, MatTabsModule, MatButtonModule, MatSelectModule} from '@angular/material';
+import {MatInputModule, MatTabsModule, MatButtonModule, MatSelectModule, MatToolbarModule} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRoutingModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 import { ContainerComponent } from './container/container.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ContainerComponent
+    ContainerComponent,
+    DashboardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule ,
     MatInputModule,
     MatTabsModule,
     MatButtonModule,
-    MatSelectModule
+    MatSelectModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
